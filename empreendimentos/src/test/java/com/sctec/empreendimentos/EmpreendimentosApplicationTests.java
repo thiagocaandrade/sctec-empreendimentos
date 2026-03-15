1b80acab811ed6ab80f2f0ae8b8d4ad919bb3892
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2026-03-15 20:43:25 -0300

    commit initial

---
+++ empreendimentos/src/test/java/com/sctec/empreendimentos/EmpreendimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sctec.empreendimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpreendimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
